ed4cef5b73709ce52c33d80d77a1de775c7ff4f4
Danilo dos Santos Andrade <danilo.s.andrade@gmail.com>
2017-01-02 10:41:43 -0200

    SecurityManager initialization #7

---
+++ src/main/java/br/com/caelum/vraptor/security/produces/VRaptorShiroInit.java
@@ -0,0 +1,16 @@
+package br.com.caelum.vraptor.security.produces;
+
+/**
+ * Created by danilo on 02/01/17.
+ */
+public class VRaptorShiroInit {
+	private final String message;
+
+	public VRaptorShiroInit(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
